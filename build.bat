17f429f29bd614f2d6863edaad74db800739a03c
Balázs Keresztury <b@belidzs.hu>
2021-07-01 18:44:25 +0200

    Build on Linux

--- build.bat
+++
@@ -1,4 +0,0 @@
-set VERSION=2.7.2
-docker build . --no-cache --platform arm -t belidzs/duplicacy-cli-alpine:latest -t belidzs/duplicacy-cli-alpine:%VERSION% --build-arg %VERSION%
-docker push belidzs/duplicacy-cli-alpine:latest
-docker push belidzs/duplicacy-cli-alpine:%VERSION%
