cbba41fff333a463d58094c4f9e4277ab0d0543d
Balázs Keresztury <b@belidzs.hu>
2021-07-03 11:27:25 +0200

    Set push format

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DUPLICACY_VERSION=2.7.2
 podman build . --no-cache -t docker.io/belidzs/duplicacy-cli-alpine:latest -t "docker.io/belidzs/duplicacy-cli-alpine:$DUPLICACY_VERSION" --build-arg "VERSION=$DUPLICACY_VERSION"
-podman push docker.io/belidzs/duplicacy-cli-alpine:latest
-podman push "docker.io/belidzs/duplicacy-cli-alpine:$DUPLICACY_VERSION"
+podman push --format=docker docker.io/belidzs/duplicacy-cli-alpine:latest
+podman push --format=docker "docker.io/belidzs/duplicacy-cli-alpine:$DUPLICACY_VERSION"
