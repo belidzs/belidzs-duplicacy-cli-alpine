b636459e3fb109022ce9d7ea8a28131bf5f2458d
Balázs Keresztury <b@belidzs.hu>
2023-10-07 14:51:33 +0200

    Bump version

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-set VERSION=3.2.2
+set VERSION=3.2.3
 docker buildx build . --no-cache --platform linux/amd64,linux/arm,linux/arm64 --tag belidzs/duplicacy-cli-alpine:latest --tag belidzs/duplicacy-cli-alpine:%VERSION% --build-arg VERSION=%VERSION% --push
